cf76dddc96ab2e657a40ca8f0e8e45ca6aeb542d
Enes Kaan <105228606+Ekgzl@users.noreply.github.com>
2024-03-15 16:23:50 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+class clockType
+        {
+        private:
+            int hr;
+            int min;
+            int sec;
+
+        public:
+            void setTime(int hr,int min,int sec)
+            {
+                this->hr = hr;
+                this->min = min;
+                this->sec = sec;
+            }
+
+            void getTime(int& hr, int& min, int& sec)const
+            {
+                hr = this->hr;
+                min = this->min;
+                sec = this->sec;
+            }
+
+            void printTime()const
+            {
+                cout << hr << "." << min << "." << sec <<endl;
+            }
+
+            int incrementSeconds()
+            {
+                if(sec==59)
+                {
+                    incrementMinutes();
+                    sec = 0;
+                }
+                else
+                    sec++;
+
+                return sec;
+            }
+
+
+            int incrementMinutes()
+            {
+                if(min==59)
+                {
+                    min = 0;
+                    incrementHours();
+                }
+                else
+                    min++;
+
+                return min;
+            }
+
+            int incrementHours()
+            {
+                if(hr==23)
+                    hr = 0;
+                else
+                    hr++;
+
+                return hr;
+            }
+
+            clockType(int hr,int min,int sec) : hr(hr),min(min),sec(sec) {}
+            clockType() : hr(0),min(0),sec(0) {}
+
+        };
+
+int main()
+{
+// Parametreli kurucu fonksiyon kullanarak bir saat oluştur
+    clockType myClock(12, 30, 58);
+
+    // Zamanı yazdır
+    myClock.printTime();
+
+    // Saniyeyi artır
+    myClock.incrementSeconds();
+    myClock.incrementSeconds();
+
+    // Zamanı tekrar yazdır
+    myClock.printTime();
+
+    return 0;
+
+
+}
